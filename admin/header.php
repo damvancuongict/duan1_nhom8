2e4d504e2584b231dcb154303e83422468584107
KHAIPH29333 <khaindph29333@fpt.edu.vn>
2023-07-13 00:29:51 +0700

    Đã update

---
+++ admin/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="boxcenter">
+        <div class="row mb headeradm">
+            <H1>ADMIN</H1>
+        </div>
+        <div class="row mb menu">
+            <ul>
+                <li><a href="#">Trang chủ</a> </li>
+                <li><a href="index.php?act=adddm">Danh mục</a> </li>
+                <li><a href="#">Khóa học</a> </li>
+                <li><a href="#">Người dùng</a> </li>
+                <li><a href="#">Bình luận</a> </li>
+                <li><a href="#">Thống kê</a> </li>
+            </ul>
+        </div>
---
+++ admin/home.php
@@ -0,0 +1,23 @@
+<div class="row">
+            <div class="row formtitle">
+                <h1>THÊM MỚI LOẠI KHÓA HỌC</h1>
+            </div>
+            <div class="row formcontent">
+                <form action="#" method="post">
+                    <div class="row mb10">
+                        Mã khóa học <br>
+                        <input type="text" name="makhoahoc" disabled>
+                    </div>
+                    <div class="row mb10">
+                        Tên khóa học <br>
+                        <input type="text" name="tenkhoahoc" id="">
+                    </div>
+                    <div class="row mb10">
+                        <input type="submit" value="THÊM MỚI">
+                        <input type="reset" value="NHẬP LẠI">
+                        <a href="danhsach.html"><input type="button" value="DANH SÁCH"></a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
