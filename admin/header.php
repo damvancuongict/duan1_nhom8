3cbae41a907b80806761806140c56837c04c9f42
Dam Cuong <cuongdvph30239@fpt.edu.vn>
2023-07-14 18:18:02 +0700

    hhaha

--- admin/header.php
+++ admin/header.php
@@ -1,19 +1,19 @@
-<!DOCTYPE html>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
     <link rel="stylesheet" href="style.css">
+    <title>admin</title>
 </head>
-<body><div class="row mb headeradm">
-            <H1>ADMIN</H1>
-        </div>
+<body>
     <div class="boxcenter">
-        
+        <div class="row mb headeradmin">
+            <h1>QUẢN TRỊ WEBSITE</h1>
+        </div>
         <div class="row mb menu">
-            <ul>
+        <ul>
                 <li><a href="#">Trang chủ</a> </li>
                 <li><a href="index.php?act=adddm">Danh mục</a> </li>
                 <li><a href="index.php?act=addkh">Khóa học</a> </li>
--- admin/home.php
+++ admin/home.php
@@ -1,25 +1,22 @@
 
-<div class="row">
-            <div class="row formtitle">
-                <h1>THÊM MỚI LOẠI KHÓA HỌC</h1>
-            </div>
-            <div class="row formcontent">
-                <form action="#" method="post">
-                    <div class="row mb10">
-                        Mã khóa học <br>
-                        <input type="text" name="makhoahoc" disabled>
-                    </div>
-                    <div class="row mb10">
-                        Tên khóa học <br>
-                        <input type="text" name="tenkhoahoc" id="">
-                    </div>
-                    <div class="row mb10">
+
+    <div class="row header" ><h1>THÊM MỚI LOẠI KHÓA HỌC</h1></div>
+        <div class="row formcontend" >
+            <form action="#" method="post">
+                <div class="row mb">
+                    <label for="">Mã khóa học</label><br>
+                     <input type="text" name="makhoahoc" id="" disabled>
+                </div>
+                <div class="row mb10">
+                    <label for="">Tên khóa học</label><br>
+                     <input type="text" name="tenkhoahoc" id="" >
+                </div>
+                <div class="row mb10">
                         <input type="submit" value="THÊM MỚI">
                         <input type="reset" value="NHẬP LẠI">
                         <a href="index.php?act=listdm"><input type="button" value="DANH SÁCH"></a>
                     </div>
-                </form>
-            </div>
+            </form>
         </div>
-    </div>
+       
     
